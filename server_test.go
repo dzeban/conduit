2731406b2a8a88f257884cc7c962566222434159
Alex Dzyoba <alex@dzyoba.com>
2019-02-18 23:46:54 +0300

    Add test for not found article

--- server_test.go
+++ server_test.go
@@ -3,9 +3,10 @@ package main
 import (
 	"testing"
 
-	"github.com/dzeban/conduit/mock"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/dzeban/conduit/mock"
 )
 
 func TestArticlesList(t *testing.T) {
@@ -20,4 +21,16 @@ func TestArticlesList(t *testing.T) {
 	assert.HTTPRedirect(t, router.ServeHTTP, "GET", "/articles", nil)
 	assert.HTTPSuccess(t, router.ServeHTTP, "GET", "/articles/", nil)
 	assert.HTTPBodyContains(t, router.ServeHTTP, "GET", "/articles/", nil, "Title 1")
+}
+
+func TestNotFoundArticle(t *testing.T) {
+	mockArticlesService, _ := mock.New()
+	s := Server{
+		articles: mockArticlesService,
+	}
+
+	router := gin.New()
+	router.GET("/articles/", s.HandleArticles)
+
+	assert.HTTPError(t, router.ServeHTTP, "GET", "/articles/xxx", nil)
 }
