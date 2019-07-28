801555ec01dab0c77433e23319e915e46a4f52a5
Alex Dzyoba <alex@dzyoba.com>
2019-07-28 15:33:28 +0300

    service/user: Add test for Register handler

--- service/user/handlers_test.go
+++ service/user/handlers_test.go
@@ -66,6 +66,60 @@ func TestLoginHandler(t *testing.T) {
 	}
 }
 
+func TestHandleUserRegister(t *testing.T) {
+	cases := []struct {
+		name   string
+		body   string
+		status int
+	}{
+		{
+			"null",
+			"",
+			http.StatusBadRequest,
+		},
+		{
+			"empty",
+			"{}",
+			http.StatusBadRequest,
+		},
+		{
+			"conflict",
+			`{"user":{"email":"test@example.com","username": "test", "password":"test"}}`,
+			http.StatusConflict,
+		},
+		{
+			"invalid",
+			`{"user":{"email":"new@example.com","password":"new"}}`,
+			http.StatusBadRequest,
+		},
+		{
+			"valid",
+			`{"user":{"email":"new@example.com","username": "new", "password":"new"}}`,
+			http.StatusCreated,
+		},
+	}
+
+	s := New(newMockStore(), testSecret)
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+
+			req := httptest.NewRequest("POST", "/", strings.NewReader(c.body))
+			s.HandleUserRegister(rr, req)
+
+			if rr.Code != c.status {
+				t.Errorf("unexpected status, want %d, got %d", c.status, rr.Code)
+				t.Error(rr.Body.String())
+			}
+
+			if rr.Code == http.StatusOK {
+				checkToken(rr.Body.Bytes(), t)
+			}
+		})
+	}
+}
+
 func checkToken(body []byte, t *testing.T) {
 	type resp struct {
 		User struct {
@@ -87,69 +141,6 @@ func checkToken(body []byte, t *testing.T) {
 		t.Errorf("invalid token '%s': %s", r.User.Token, err)
 	}
 }
-
-// func TestHandleUserRegister(t *testing.T) {
-// 	tests := []struct {
-// 		name     string
-// 		data     string
-// 		status   int
-// 		response *userResponse
-// 	}{
-// 		{
-// 			name:   "null",
-// 			data:   "",
-// 			status: http.StatusBadRequest,
-// 		},
-// 		{
-// 			name:   "empty",
-// 			data:   "{}",
-// 			status: http.StatusBadRequest,
-// 		},
-// 		{
-// 			name:   "conflict",
-// 			data:   `{"user":{"email":"test@example.com","username": "test", "password":"test"}}`,
-// 			status: http.StatusConflict,
-// 		},
-// 		{
-// 			name:   "invalid",
-// 			data:   `{"user":{"email":"newuser@example.com","password":"newuser"}}`,
-// 			status: http.StatusBadRequest,
-// 		},
-// 		{
-// 			name:   "valid",
-// 			data:   `{"user":{"email":"newuser@example.com","username": "newuser", "password":"newuser"}}`,
-// 			status: http.StatusCreated,
-// 			response: &userResponse{
-// 				User: userTest{
-// 					Name:  "newuser",
-// 					Email: "newuser@example.com",
-// 				},
-// 			},
-// 		},
-// 	}
-
-// 	for _, test := range tests {
-// 		t.Run(test.name, func(t *testing.T) {
-// 			resp, err := http.Post(testServer.URL+"/users/", "application/json", strings.NewReader(test.data))
-// 			if err != nil {
-// 				t.Error("failed to make a request:", err)
-// 			}
-// 			defer resp.Body.Close()
-
-// 			body, _ := ioutil.ReadAll(resp.Body)
-// 			if test.status != resp.StatusCode {
-// 				t.Errorf("invalid status code: expected %d, got %d", test.status, resp.StatusCode)
-// 			}
-
-// 			if test.response != nil {
-// 				err = checkResponse(test.response, body)
-// 				if err != nil {
-// 					t.Error(err)
-// 				}
-// 			}
-// 		})
-// 	}
-// }
 
 // func TestHandleUserGet(t *testing.T) {
 // 	response, err := registerTestUser("testUserGet@example.com", "testUserGet", "password")
